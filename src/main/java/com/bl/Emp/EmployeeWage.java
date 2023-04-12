652ea6f7148288badbf748e0fd01754a9257009e
yogeshshende123 <yogeshshende1109@gmail.com>
2023-04-12 08:48:32 +0530

    Added Employee Attendnce

--- src/main/java/com/bl/Emp/EmployeeWage.java
+++ src/main/java/com/bl/Emp/EmployeeWage.java
@@ -2,6 +2,16 @@ package com.bl.Emp;
 
 public class EmployeeWage {
     public static void main(String[] args){
+
         System.out.println("Wellcome to Employee wage project");
+        //  UC1_check employee is present or not.
+        int ispresent=1;
+        int emp=(int) (Math.random()*10)%2;
+        if ( ispresent==emp)
+            System.out.println("Employee is present");
+        else
+            System.out.println("Employee is not present");
+        //UC_2g
     }
+
 }
